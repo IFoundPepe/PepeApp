d9b43b6c0c409bef24612a11852c2016b3abfa80
IFoundPepe <grwilson@gmail.com>
2017-07-30 14:51:30 -0700

    pepe_control UI launches!
    PepeControlActivity added as an intent of the DeviceScanActivity.  Now
    the control UI launches when you select your Pepe device.

--- Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceScanActivity.java
@@ -148,9 +148,9 @@ public class DeviceScanActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
         if (device == null) return;
-        final Intent intent = new Intent(this, DeviceControlActivity.class);
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+        final Intent intent = new Intent(this, PepeControlActivity.class);
+        intent.putExtra(PepeControlActivity.EXTRAS_DEVICE_NAME, device.getName());
+        intent.putExtra(PepeControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
         if (mScanning) {
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
             mScanning = false;
